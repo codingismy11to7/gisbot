eb01634ccdfec8a60c06bbde4777d80d6662ca32
Steven Scott <steven.scott@codemettle.com>
2024-06-21 23:36:20 -0400

    parser

---
+++ src/parser/index.test.ts
@@ -0,0 +1,35 @@
+import { Option } from "effect";
+import { getSearchTextAndIndex } from "./index";
+
+const run = (s: string) => getSearchTextAndIndex(s).pipe(Option.getOrUndefined);
+
+describe("parser", () => {
+  it("all options work", () => {
+    expect(run("gisa42    xyz  ")).toEqual({ index: 42, text: "xyz animated gif" });
+  });
+
+  it.each([
+    ["g", "girls"],
+    ["t", "then and now"],
+    ["i", "infographic"],
+    ["a", "animated gif"],
+    ["m", "meme"],
+    ["", ""],
+  ])("'%s' mod is handled", (mod, searchSuff) => {
+    expect(run(`gis${mod} a`)?.text).toEqual(`a${!searchSuff.length ? "" : ` ${searchSuff}`}`);
+  });
+
+  it("index works", () => {
+    expect(run("gis x")?.index).toEqual(1);
+    expect(run("gis1 x")?.index).toEqual(1);
+    expect(run("gis42 x")?.index).toEqual(42);
+    expect(run("gisi42 x")?.index).toEqual(42);
+  });
+
+  it("no search string is none", () => {
+    expect(run("gis ")).toBeUndefined();
+    expect(run("gisi ")).toBeUndefined();
+    expect(run("gis      ")).toBeUndefined();
+    expect(run("gisa42    ")).toBeUndefined();
+  });
+});
--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,5 +1,6 @@
 import { Match, Option, pipe } from "effect";
 
+type Mod = "g" | "t" | "i" | "a" | "m" | undefined;
 const re = /^gis([gtiam])?(\d+)? (.+)/i;
 
 const parseNum = (n: string | undefined) =>
@@ -13,17 +14,18 @@ const parseNum = (n: string | undefined) =>
 export const getSearchTextAndIndex = (messageText: string) =>
   pipe(
     Option.fromNullable(re.exec(messageText)),
-    Option.andThen(res => ({ mod: res[1], idx: parseNum(res[2]), search: (res[3] ?? "").trim() })),
+    Option.andThen(res => ({ mod: res[1] as Mod, idx: parseNum(res[2]), search: (res[3] ?? "").trim() })),
     Option.filter(x => !!x.search.length),
     Option.andThen(({ mod, idx, search }) => ({
       index: idx ?? 1,
       text: `${search}${Match.value(mod).pipe(
+        Match.when(undefined, () => ""),
         Match.when("g", () => " girls"),
         Match.when("t", () => " then and now"),
-        Match.when("i", () => " infographics"),
+        Match.when("i", () => " infographic"),
         Match.when("a", () => " animated gif"),
         Match.when("m", () => " meme"),
-        Match.orElse(() => ""),
+        Match.exhaustive,
       )}`,
     })),
   );
